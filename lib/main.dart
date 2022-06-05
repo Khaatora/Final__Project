7d7767fc3377651c8e82b52344e823dc9d012b58
Khaatora <ahosari20@gmail.com>
2022-06-05 20:00:45 +0200

    added firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
+  runApp(new MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(new MyApp());
-
 
 }
 
